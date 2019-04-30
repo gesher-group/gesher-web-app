7a0f609ff81477d891738e30f6caa9272c54fd3e
kyle1668 <kyledevinobrien1@gmail.com>
2019-04-30 12:06:54 -0700

    added routes, prettier, and eslint

---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import App from '../components/App';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import Articles from '../components/Articles';
+
+export default () => {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={App} />
+				<Route path="/about" exact component={About} />
+				<Route path="/articles" exact component={Articles} />
+				<Route path="/contact" exact component={Contact} />
+			</Switch>
+		</BrowserRouter>
+	);
+};
