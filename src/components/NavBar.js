24e3519fb292ffd293d4e5df7c15facfdc69db75
kyle1668 <kyledevinobrien1@gmail.com>
2019-04-30 14:26:44 -0700

    began to style navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,19 @@ import routesData from "../routes/routes-list.json";
 
 const navItems = () =>
   routesData.routes.map((currentRoute, currentIndex) => (
-    <li className="navbar-item" key={currentIndex}>
+    <li className="navbar_item" key={currentIndex}>
       <a href={currentRoute.path}>{currentRoute.routeName}</a>
     </li>
   ));
 
 const NavBar = () => (
-  <header className="NavBar">
-    <nav>
-      <div className="hamburger-icon" />
-      <div>
+  <header className="navbar">
+    <nav className="navbar_content">
+      <div className="hamburger_icon" />
+      <div className="navbar_logo">
         <a href="/">Gesher Group Logo</a>
       </div>
-      <div className="nav-items">{navItems()}</div>
+      <div className="nav_items">{navItems()}</div>
     </nav>
   </header>
 );
