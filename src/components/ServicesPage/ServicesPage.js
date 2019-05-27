c78cd80d5e82af644d1097403b8e92a8a1d32822
kyle1668 <kyledevinobrien1@gmail.com>
2019-05-27 13:35:51 -0700

    added responsive design for services section

--- src/components/ServicesPage/ServicesPage.js
+++ src/components/ServicesPage/ServicesPage.js
@@ -1,10 +1,46 @@
 import React from "react";
 import Menu from "../Menu/Menu";
 
+import "./ServicesPage.css";
+
 const ServicesPage = () => {
   return (
     <div className="services-page">
       <Menu />
+
+      <div className="page-content-header">
+        <h1>Our Services</h1>
+      </div>
+
+      <div className="services-section">
+        <div className="gesher-service">
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{" "}
+          </p>
+        </div>
+        <div className="gesher-service">
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{" "}
+          </p>
+        </div>
+        <div className="gesher-service">
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{" "}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
